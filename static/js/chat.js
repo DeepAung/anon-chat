87444e121dadb1431619e0c58b09598217bdc3f7
DeepAung <i.deepaung@gmail.com>
2024-04-08 16:53:46 +0700

    style: style everything

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,12 +1,4 @@
-window.onload = () => {
-	console.log("add event beforeunload");
-	window.addEventListener("beforeunload", (e) => {
-		e.preventDefault();
-		console.log("hello world beforeunload");
-		e.returnValue = true;
-	});
-};
-
-document.addEventListener("htmx:wsClose", (e) => {
-	// window.location.assign("/");
+document.addEventListener("htmx:wsClose", (_) => {
+	console.log("after htmx:wsClose");
+	window.location.assign("/");
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+// const { colors } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./views/**/*.{html,js,templ,go}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgb(94, 234, 212)",
+      },
+    },
     fontFamily: {
       mono: ["Fira Code", "ui-monospace", "SFMono-Regular"],
     },
